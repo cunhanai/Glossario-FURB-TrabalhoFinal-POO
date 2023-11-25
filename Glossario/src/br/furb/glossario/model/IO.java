617434c9b95f0709432f9447940b3c4a97799d5c
Gabriel Cestari <grcestari@gmail.com>
2023-11-25 19:16:57 -0300

    add IO

---
+++ Glossario/src/br/furb/glossario/model/IO.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.furb.glossario.model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author bielc
+ */
+public class IO {
+
+    private IO() {
+    }
+
+    public static void saveData(String data, String envio) throws IOException {
+        if (envio == null || envio.isBlank()) {
+            throw new IllegalArgumentException("envio null/vazio");
+        }
+
+        Path pathEnvio = Paths.get(envio);
+        if (!Files.exists(pathEnvio)) {
+            if (!Files.exists(pathEnvio.getParent())) {
+                Files.createDirectories(pathEnvio.getParent());
+            }
+
+            Files.createFile(pathEnvio);
+        }
+
+        File fileEnvio = pathEnvio.toFile();
+
+        try (PrintWriter impressora = new PrintWriter(fileEnvio, "UTF-8")) {
+            impressora.print(data);
+
+        }
+    }
+
+    public static String readBufferedReader(String base) throws IOException {
+        if (base == null || base.isBlank()) {
+            throw new IllegalArgumentException("base null/vazio");
+        }
+
+        File fileBase = new File(base);
+
+        if (!fileBase.exists()) {
+            return null;
+        } else if (!fileBase.isFile()) {
+            throw new IllegalArgumentException("base nn existe ou nn eh arquirvo");
+        }
+        ArrayList<String> lines = new ArrayList<>();
+        String line;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileBase, Charset.forName("UTF-8")))) {
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return String.join("\n", lines);
+    }
+
+    public static String readScannerData(String base) throws IOException {
+        if (base == null || base.isBlank()) {
+            throw new IllegalArgumentException("base null/vazio");
+        }
+
+        File fileBase = new File(base);
+
+        if (!fileBase.exists()) {
+            return null;
+        } else if (!fileBase.isFile()) {
+            throw new IllegalArgumentException("base nn existe ou nn eh arquirvo");
+        }
+        ArrayList<String> lines = new ArrayList<>();
+        try (Scanner scanner = new Scanner(fileBase, "UTF-8")) {
+            while (scanner.hasNext()) {
+                lines.add(scanner.nextLine());
+            }
+        }
+        return String.join("\n", lines);
+    }
+}
