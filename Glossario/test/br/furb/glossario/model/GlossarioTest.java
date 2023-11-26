68c7d6325e421b22822f9097342109ab879647b2
Gabriel Cestari <grcestari@gmail.com>
2023-11-26 20:48:34 -0300

    test packages

---
+++ Glossario/test/br/furb/glossario/model/GlossarioTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package br.furb.glossario.model;
+
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author bielc
+ */
+public class GlossarioTest {
+    
+    public GlossarioTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
---
+++ Glossario/test/br/furb/glossario/model/data/IOTest.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package br.furb.glossario.model.data;
+
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author bielc
+ */
+public class IOTest {
+    
+    private IO io;
+    
+    public IOTest() {
+    }
+    
+    @Before
+    public void setUp(){
+        io = new IO();
+    }
+    
+    @After
+    public void tearDown() {
+        
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
